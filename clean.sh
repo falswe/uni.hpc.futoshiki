c0a109446be1c71abc92adc913c5bca1ec2c10a6
falswe <w.falschlunger@studenti.unitn.it>
2025-06-16 18:56:54 +0200

    update clean.sh to latest execution numbers

--- clean.sh
+++ clean.sh
@@ -3,5 +3,5 @@ find . -type f -name "*.sh.e*" -delete
 find . -type f -name "*.sh.o*" -delete
 find . -type f -name "*.o" -delete
 find . -type f ! -name "*.*" -not \( -path '*/.*' \) -delete
-find . -type f -name "*.o28*" -delete
-find . -type f -name "*.e28*" -delete
+find . -type f -name "*.o32*" -delete
+find . -type f -name "*.e32*" -delete
