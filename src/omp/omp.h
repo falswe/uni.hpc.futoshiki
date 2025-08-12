d9209d7e676c98bc4facc0143225bcbae32868a9
falswe <w.falschlunger@studenti.unitn.it>
2025-08-12 10:04:34 +0200

    Even safer include guards for futoshiki's omp.h

--- src/omp/omp.h
+++ src/omp/omp.h
@@ -1,5 +1,5 @@
-#ifndef _OMP_H
-#define _OMP_H
+#ifndef FUTOSHIKI_OMP_H
+#define FUTOSHIKI_OMP_H
 
 #include "../common/utils.h"
 
@@ -31,4 +31,4 @@ bool omp_solve(Futoshiki* puzzle, int solution[MAX_N][MAX_N]);
  */
 void omp_set_task_factor(double factor);
 
-#endif  // _OMP_H
+#endif  // FUTOSHIKI_OMP_H
